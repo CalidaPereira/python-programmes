454fb6f5e959db467e82cf4f0d806123effbb173
CalidaPereira <39001434+CalidaPereira@users.noreply.github.com>
2020-10-10 15:49:44 +0200

    Data Structures refresher - stacks

---
+++ stack.py
@@ -0,0 +1,20 @@
+# Python stack functions
+#Coded with reference to Joe Marini's course on LinkedIn
+
+# TODO: create a new empty stack
+stack = []
+
+# TODO: push items onto the stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+# TODO: print the stack contents
+print(stack)
+
+# TODO: pop an item off the stack
+x = stack.pop()     #Without argument
+print(stack)
+y = stack.pop(1)    #With argument
+print(stack)
